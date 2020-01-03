95a1c77e942bad893455b7a4205ea2f0e123e2f1
Kaustav Paul <roni0989@gmail.com>
2020-01-03 19:19:22 +0530

    commit

--- classes.js
+++ classes.js
@@ -7,6 +7,14 @@ Car.prototype.drive = function(){
     return 'vroom'
 }
 
+function Toyota(options)
+{
+    Car.call(this,options)
+    this.category = "vehicle"
+}
+
 const car = new Car({title:"bmw"})
 console.log(car);
 console.log(car.drive());
+const titleT = new Toyota({title:"hello"}).title
+console.log(titleT);
