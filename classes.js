a8a795f10235e921203a35d1565913eb8ce6b1d8
Kaustav Paul <roni0989@gmail.com>
2020-01-03 19:46:46 +0530

    master

--- classes.js
+++ classes.js
@@ -1,3 +1,4 @@
+//ES5 representation
 function Car(options)
 {
     this.title = options.title
@@ -18,3 +19,24 @@ console.log(car);
 console.log(car.drive());
 const titleT = new Toyota({title:"hello"}).title
 console.log(titleT);
+
+console.log("---------------------")
+
+//ES6 representation
+
+class Toy
+{
+    constructor(options)
+    {
+        this.title = options.title
+    }
+    drive()
+    {
+        return 'Beep beep'
+    }
+}
+
+const toy = new Toy({'title':"Toyota"});
+const {title} = toy;
+console.log(title);
+console.log(toy.drive());
