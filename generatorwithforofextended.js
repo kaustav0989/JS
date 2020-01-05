158f347aa3aa5dcd16c4edf350239ed2df15d144
Kaustav Paul <roni0989@gmail.com>
2020-01-05 13:12:08 +0530

    master

--- generatorwithforofextended.js
+++ generatorwithforofextended.js
@@ -1,18 +1,34 @@
+const testingTeam = {
+    lead:"Kaustav",
+    tester:"spandana"
+}
+
 const engineeringTeam = {
+    testingTeam,
     lead:"suprith",
     manager:"anusha",
     team:"container"
 }
 
-function* iterator(teamD)
+function* Teamiterator(teamD)
 {
     yield teamD.lead;
     yield teamD.manager;
     yield teamD.team;
+
+    //Delegation of genertors
+    const testingTeamGenerator = testingTeamiterator(teamD.testingTeam);
+    yield* testingTeamGenerator;
+}
+
+function* testingTeamiterator(teamD)
+{
+    yield teamD.lead;
+    yield teamD.tester;
 }
 
 const members = [];
-for( let member of iterator(engineeringTeam) )
+for( let member of Teamiterator(engineeringTeam) )
 {
     members.push(member);
 } 
